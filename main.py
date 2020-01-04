c45571bbb98b70484e1cd75f71a5631b2712ca4f
EatingSugar <whiteleya-y15@mgs.org>
2020-01-04 21:19:45 +0000

    Task 3 completed

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ categories = ["Phone", "Phone", "Phone", "Phone", "Phone", "Phone", "Tablet", "T
 itemcodes = ["BPCM", "BPSH", "RPSS", "RPLL", "YPLS", "YPLL", "RTMS", "RTLM", "YTLM", "YTLL", "SMNO", "SMPG", "CSST", "CSLX", "CGCR", "CGHM"]
 descs = ["Compact", "Clam Shell", "RoboPhone – 5-inch screen and 64 GB memory", "RoboPhone – 6-inch screen and 256 GB memory", "Y-Phone Standard – 6-inch screen and 64 GB memory", "Y-Phone Deluxe – 6-inch screen and 256 GB memory", "RoboTab – 8-inch screen and 64 GB memory", "RoboTab – 10-inch screen and 128 GB memory", "Y-Tab Standard – 10-inch screen and 128 GB memory", "Y-Tab Deluxe – 10-inch screen and 256 GB memory", "SIM Free", "Pay As You Go", "Standard", "Luxury", "Car charger", "Home charger"]
 prices = [29.99, 49.99, 199.99, 499.99, 549.99, 649.99, 149,99, 299.99, 499.99, 599.99, 0, 9.99, 0, 50, 19.99, 15.99]
-pricetotal = 0
+phonepricetotal = 0
+overallphonepricetotal = 0
 itemspurchased = []
+phonesbought = 0
 additionalphones = True
 
 while additionalphones == True: 
+  phonepricetotal = 0
   phoneortablet = input("Do you want a 'phone' or 'tablet'? ")
   while phoneortablet != "phone" and phoneortablet != "tablet":
     phoneortablet = input("Do you want a 'phone' or 'tablet'? ")
@@ -26,7 +29,7 @@ while additionalphones == True:
 
   useritem = input("Input the item code of the phone/tablet you want to buy. ")
   itemindex = itemcodes.index(useritem)
-  pricetotal += prices[itemindex]
+  phonepricetotal += prices[itemindex]
   itemspurchased.append(descs[itemindex])
 
   simorPAYG = input("Do you want 'SIM Free' or 'Pay As You Go'? ")
@@ -34,7 +37,7 @@ while additionalphones == True:
     simorPAYG = input("Do you want a 'SIM' or 'Pay As You Go'? ")
 
   simindex = descs.index(simorPAYG)
-  pricetotal += prices[simindex]
+  phonepricetotal += prices[simindex]
   itemspurchased.append(descs[simindex])
 
   casechoice = input("'Standard' or 'Luxury' case? ")
@@ -42,33 +45,37 @@ while additionalphones == True:
     casechoice = ("'Standard' or 'Luxury' case? ")
 
   caseindex = descs.index(casechoice)
-  pricetotal += prices[caseindex]
+  phonepricetotal += prices[caseindex]
   itemspurchased.append(descs[caseindex])
 
   numberofchargers = int(input("How many chargers? Input '0' for none, '1' for a car charger, '2' for a home charger,  or '3' for both. "))
   while numberofchargers not in range(0, 3):
     numberofchargers = int(input("How many chargers? Input '0' for none, '1' for a car charger, '2' for a home charger,  or '3' for both. "))
+  
+  if phonesbought > 0:
+    phonepricetotal = phonepricetotal * 0.9
 
   if numberofchargers == 0:
     pass
   elif numberofchargers == 3:
     itemspurchased.append(descs[14])
     itemspurchased.append(descs[15])
-    pricetotal += (prices[14] + prices[15])
+    phonepricetotal += (prices[14] + prices[15])
   else:
     itemspurchased.append(descs[numberofchargers + 13])
-    pricetotal += prices[numberofchargers + 13]
+    phonepricetotal += prices[numberofchargers + 13]
 
   extraphone = input("Do you want an extra phone? [Y/N]")
   while extraphone != "Y" and extraphone != "N":
     extraphone = input("Do you want an extra phone? [Y/N]")
   if extraphone == "N":
     additionalphones = False
+    overallphonepricetotal += phonepricetotal
   else:
-    pass
+    overallphonepricetotal += phonepricetotal
 
 print()
-print("Total price: $" + str(pricetotal))
+print("Total price: $" + str(phonepricetotal))
 print("Items purchased: " + str(itemspurchased))
   
   
